2af95710af6a499d4cd6b7802bcd73da287f256f
staiber <staiber1986@gmail.com>
2021-09-13 22:28:30 +0300

    intital commit

---
+++ digit_converter.py
@@ -0,0 +1,11 @@
+user_input = int(input('Input an integer to convert: '))
+
+print("Number {0}:\n"
+      "\n"
+      " - in binary(двоичная)        {0:0b} \n"
+
+      " - in octal(восьмеричная)     {0:0o} \n"
+      
+      " - in hex(шестнадцатеричная)  {0:0x}\n".format(user_input))
+
+input("Нажмите \"Enter\" для закрытия программы.")
